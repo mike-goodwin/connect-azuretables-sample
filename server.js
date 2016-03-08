076b526adbc92ee3929de730d76e8f9f6fa7c1b7
mike-goodwin <mike.goodwin@owaps.org>
2016-03-08 22:54:36 +0000

    test without debug

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 ﻿#!/usr/bin/env node
-var debug = require('debug')('connect-azuretables-sample');
+//var debug = require('debug')('connect-azuretables-sample');
 var app = require('./app');
 
 app.set('port', process.env.PORT || 3000);
 
 var server = app.listen(app.get('port'), function() {
-    debug('Express server listening on port ' + server.address().port);
+    console.log('Express server listening on port ' + server.address().port);
+    //debug('Express server listening on port ' + server.address().port);
 });
